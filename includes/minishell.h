9a66386fd9ea16601efd2f75b679510530ae5293
Nathan Lecreux <nlecreux@c2r9p2.42lehavre.fr>
2025-02-22 20:00:41 +0100

    includes

---
+++ includes/minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlecreux <nlecreux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 17:19:18 by nlecreux          #+#    #+#             */
+/*   Updated: 2025/02/22 17:43:09 by nlecreux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL
+# define MINISHELL
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <utmp.h>
+# include <term.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../libft/includes/libft.h"
+
+# endif
