a6377bddda024ad0bd1e1ab3bd3977de32f3c58b
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-02-24 12:01:44 +0100

    remove clear fonction

--- srcs/parsing/signal_handler.c
+++ srcs/parsing/signal_handler.c
@@ -12,27 +12,6 @@
 
 #include "../../includes/minishell.h"
 
-/**
- * @brief Comme son nom l'indique, cette fonction permet de désactiver l affichage des signaux de  contrôle (ctrl-c, ctrl-d,..)
- * La structure de type termios permet de stocker les paramètres du terminal
- * La fonction tcgetattr récupère les attributs actuels du terminal associé à l'entrée standard (clavier).
- * termios_p.c_lflag &= ~ECHOCTL désactive l'affichage des caractères de contrôle (comme ^C, ^D) en supprimant l'option ECHOCTL.
- * La fonction tcsetattr applique immédiatement les nouvelles configurations au terminal.
- */
-void	disable_ctrl_echo(void)
-{
-	struct termios	termios_p;
-
-	if (tcgetattr(STDIN_FILENO, &termios_p) == -1)
-		perror("tcgetattr");
-	else
-	{
-		termios_p.c_lflag &= ~ECHOCTL;
-		if (tcsetattr(STDIN_FILENO, TCSANOW, &termios_p) == -1)
-			exit_error("tcsetattr");
-	}
-}
-
 void 	ctrl_c(int signal)
 {
 	(void)signal;
