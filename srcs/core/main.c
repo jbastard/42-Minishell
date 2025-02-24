bfafc2b8761f4604d64023e935a9522bf84047ad
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-02-24 09:18:04 +0100

    Ajout des commentaires et gestion de ctrl c et d

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,13 +6,20 @@
 /*   By: jbastard <jbastard@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 19:35:00 by jbastard          #+#    #+#             */
-/*   Updated: 2025/02/24 08:31:35 by jbastard         ###   ########.fr       */
+/*   Updated: 2025/02/24 09:14:39 by jbastard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	disable_ctrl_c_echo(void)
+/**
+ * @brief Comme son nom l'indique, cette fonction permet de désactiver l affichage des signaux de  contrôle (ctrl-c, ctrl-d,..)
+ * La structure de type termios permet de stocker les paramètres du terminal
+ * La fonction tcgetattr récupère les attributs actuels du terminal associé à l'entrée standard (clavier).
+ * termios_p.c_lflag &= ~ECHOCTL désactive l'affichage des caractères de contrôle (comme ^C, ^D) en supprimant l'option ECHOCTL.
+ * La fonction tcsetattr applique immédiatement les nouvelles configurations au terminal.
+ */
+void	disable_ctrl_echo(void)
 {
 	struct termios	termios_p;
 
@@ -35,8 +42,11 @@ void	sig_handler(int signum)
 		rl_clear_visible_line();;
 		rl_redisplay();
 	}
-	else
-		printf("%d\n", signum);
+	if (signum == 0)
+	{
+		printf("Successfully exited");
+		exit(1);
+	}
 }
 
 int		main(int ac, char **av, char **envp)
@@ -52,13 +62,13 @@ int		main(int ac, char **av, char **envp)
 
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 		exit_error("sigaction");
-	disable_ctrl_c_echo();
+	disable_ctrl_echo();
 	char *input;
 	while (1)
 	{
 		input = readline("minishell> ");
 		if (!input)
-			break;
+			sig_handler(0);
 		free(input);
 	}
 }
