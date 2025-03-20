bed11eeab0dc640325921d60c7595991e3d7c52e
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-03-20 14:14:07 +0100

    ~ | Fix probleme chemin absolu dans le check des commandes

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,7 +6,7 @@
 /*   By: nlecreux <nlecreux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 19:35:00 by jbastard          #+#    #+#             */
-/*   Updated: 2025/03/20 11:55:48 by jbastard         ###   ########.fr       */
+/*   Updated: 2025/03/20 12:26:33 by jbastard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,10 +75,8 @@ int	main()
 			break;
 		add_history(main.line);
 		if (get_cmd(&main))
-		{
-//			print_parse(main.cmd);
-			free_cmd(main.cmd);
-		}
+			if (main.cmd)
+				free_cmd(main.cmd);
 		free(main.line);
 	}
 	free(main.line);
