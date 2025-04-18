7940b7141beb8720bc3c1acc37101948be34123d
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-04-18 08:41:09 +0200

    ~ | History fixed

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -12,62 +12,15 @@
 
 #include "../../includes/minishell.h"
 
-//void	print_tokens(t_token *tokens)
-//{
-//	const char  *type_str;
-//
-//	while (tokens)
-//	{
-//		if (tokens->type == TOKEN_WORD)
-//			type_str = "WORD";
-//		else if (tokens->type == TOKEN_PIPE)
-//			type_str = "PIPE";
-//		else if (tokens->type == TOKEN_REDIR_IN)
-//			type_str = "REDIRECTION_IN";
-//		else if (tokens->type == TOKEN_REDIR_OUT)
-//			type_str = "REDIRECTION_OUT";
-//		else if (tokens->type == TOKEN_HEREDOC)
-//			type_str = "HEREDOC";
-//		else if (tokens->type == TOKEN_APPEND)
-//			type_str = "APPEND";
-//		printf("Token: %-20s | Type: %s\n", tokens->value, type_str);
-//		tokens = tokens->next;
-//	}
-//}
-//
-//void	print_parse(t_cmd	*cmd)
-//{
-//	int		i;
-//	t_cmd	*tmp;
-//	t_redir	*tmpred;
-//
-//	tmp = cmd;
-//	while (tmp)
-//	{
-//		i = 0;
-//		while (tmp->cmd_args[i])
-//			printf("Argument:   | %s\n", tmp->cmd_args[i++]);
-//		tmpred = tmp->redir;
-//		while (tmpred)
-//		{
-//			printf("Redir type: | %d\n", tmpred->type);
-//			printf("Redir file: | %s\n", tmpred->file);
-//			tmpred = tmpred->next;
-//		}
-//		printf("-----------------------\n");
-//		tmp = tmp->next;
-//	}
-//}
-
 int	handle_inputs(t_minishell *main)
 {
 	if (!main->is_here && g_signal != SIG_CHILD)
 		main->line = readline(main->prompt);
 	if (!main->line)
 		return (0);
-	add_history(main->line);
 	if (!get_cmd(main))
 		return (free(main->line), 1);
+	add_history(main->line);
 	handle_commands(main->cmd, main);
 	free_cmd(main->cmd);
 	if (main->line)
